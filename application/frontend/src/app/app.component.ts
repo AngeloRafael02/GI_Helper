dc5d38906639da64b947527a04ab4ad966c75b98
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2023-10-04 19:55:51 +0800

    integrated bootstrap CSS and AOS JS to frontend

--- application/frontend/src/app/app.component.ts
+++ application/frontend/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import * as AOS from 'aos';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'application';
+
+  constructor(private modalService: NgbModal) {
+  }
+  ngOnInit() {
+    AOS.init();
+  }
+
+  public open(modal: any): void {
+    this.modalService.open(modal);
+  }
+
+  
 }
