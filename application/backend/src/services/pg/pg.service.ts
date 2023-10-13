1b3ff7a9c13314e3d2b84c3a2e110fb3d8cbd2af
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2023-10-13 13:46:16 +0800

    fixed indentations for readability

--- application/backend/src/services/pg/pg.service.ts
+++ application/backend/src/services/pg/pg.service.ts
@@ -5,24 +5,24 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class PgService {
 
-    constructor(
-      private readonly config:ConfigService
-    ){}
+  constructor(
+    private readonly config:ConfigService
+  ){}
 
-    private readonly pool = new Pool({
-      connectionString: this.config.get<string>('DATABASE_URL'),
-      ssl: {
-        require: true,
-      },
-    })
+  private readonly pool = new Pool({
+    connectionString: this.config.get<string>('DATABASE_URL'),
+    ssl: {
+      require: true,
+    },
+  })
 
-    public async getPostgresVersion() {
-        const client = await this.pool.connect();
-        try {
-          const response = await client.query('SELECT version()');
-          console.log(response.rows[0]);
-        } finally {
-          client.release();
-        }
-      }
+  public async getPostgresVersion() {
+    const client = await this.pool.connect();
+    try {
+      const response = await client.query('SELECT version()');
+      console.log(response.rows[0]);
+    } finally {
+      client.release();
+    }
+  }
 }
