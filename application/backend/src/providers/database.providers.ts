2887c25cb4500ff7150b3b48301cb45f3bc1691a
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2023-11-02 16:00:37 +0800

    changed raw value to .env values for DB Connection

--- application/backend/src/providers/database.providers.ts
+++ application/backend/src/providers/database.providers.ts
@@ -1,5 +1,7 @@
 import { DataSource } from 'typeorm';
 import { Elements } from '../entity/elements.entity';
+import { Characters } from '../entity/character.entity';
+require('dotenv').config();
 
 export const databaseProviders = [
   {
@@ -7,12 +9,12 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'postgres',
-        host: 'ep-autumn-lab-70106029-pooler.ap-southeast-1.aws.neon.tech',
+        host: process.env['PG_HOST'],
         port: 5432,
-        username: 'recioangelorafael',
-        password: '2oyjINbO1RHE',
-        database: 'neondb',
-        entities: [Elements],
+        username: process.env['PG_USERNAME'],
+        password: process.env['PG_PASSWORD'],//'2oyjINbO1RHE',
+        database: process.env['PG_DB'],
+        entities: [Elements,Characters],
         synchronize: false,
         ssl:true,
         connectTimeoutMS:0,
