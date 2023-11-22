6d02c09e3fa25f0503828fc5471cd13548f41852
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2023-11-22 21:01:11 +0800

    removed elements query feature for testing is done

--- application/backend/src/app.module.ts
+++ application/backend/src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { ElementsController } from './controllers/elements/elements.controller';
 
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PgService } from './services/pg/pg.service';
 import { MngdbService } from './services/mngdb/mngdb.service';
 import { DatabaseModule } from './module/database.module';
-import { elementProviders,characterProviders } from './providers/elements.providers';
+import { characterProviders } from './providers/elements.providers';
 import { CharacterController } from './controllers/character/character.controller';
 import { DaysController } from './controllers/days/days.controller';
 import { WeaponsController } from './controllers/weapons/weapons.controller';
@@ -24,13 +23,11 @@ import { WeaponsController } from './controllers/weapons/weapons.controller';
   ],
   controllers: [
     AppController,
-    ElementsController,
     CharacterController,
     DaysController,
     WeaponsController
   ],
   providers: [
-    ...elementProviders,
     ...characterProviders,
     AppService,
     PgService,
--- application/backend/src/services/pg/pg.service.ts
+++ application/backend/src/services/pg/pg.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { Repository} from 'typeorm';
-import { Elements } from 'backend/src/entity/elements.entity';
 import { Pool } from 'pg'
 import { ConfigService } from '@nestjs/config';
 import { Characters } from 'backend/src/entity/character.entity';
@@ -9,47 +8,28 @@ import { Characters } from 'backend/src/entity/character.entity';
 export class PgService {
 
   constructor(
-    @Inject('ELEMENT_REPOSITORY')
-    private readonly elementReposity:Repository<Elements>,
     @Inject('CHARACTER_REPOSITORY')
     private readonly characterRepository:Repository<Characters>,
     private readonly config:ConfigService
   ){}
 
   private readonly pool = new Pool({
-    connectionString: this.config.get<string>('DATABASE_URL'),
+    connectionString: this.config.getOrThrow<string>('DATABASE_URL'),
     idleTimeoutMillis: 0,
     connectionTimeoutMillis: 0,
     ssl: {
       require: true,
     },
   })
-  private connection = this.pool.connect();
 
   public async getPostgresVersion() {
-    const client = await this.connection
+    const client = await this.pool.connect();
     try {
       const response = await client.query('SELECT version()');
       console.log(response.rows[0]);
     } finally {
       client.release();
     }
-  }
-
-  public async GetTestData() {
-    const client = await this.connection
-    try {
-      const response = await client.query(`SELECT * FROM allcharacters;`);
-      console.log(response.rows);
-      return response.rows;
-    } finally {
-      client.release();
-    }
-  }
-
-  public async getElements2():Promise<Elements[] | undefined>{
-    const result = await this.elementReposity.find();
-    return result;
   }
 
   public async getAllCharacters():Promise<Characters[]> | undefined {
@@ -72,4 +52,4 @@ export class PgService {
   }
 }
 
-//PLease Clean this code: only have one way/code to connect app to DB 
+//Please Clean this code: only have one way/code to connect app to DB 
