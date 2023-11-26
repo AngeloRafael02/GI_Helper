692b421c01c96a300c12b73dd0e4e7fac7de70b0
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2023-11-26 14:17:18 +0800

    Added getWeapon() GET Request

--- application/backend/src/controllers/weapons/weapons.controller.ts
+++ application/backend/src/controllers/weapons/weapons.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Get,Param } from '@nestjs/common';
+import { PgService } from 'backend/src/services/pg/pg.service';
 
 @Controller('weapons')
-export class WeaponsController {}
+export class WeaponsController {
+    constructor(
+        private pgService:PgService
+    ){}
+
+    @Get(':id')
+    public async getWeapon(@Param('id') id:number){
+        const Weapon = await this.pgService.getWeapon(id)
+        return {Weapon}
+    }
+}
