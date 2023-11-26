8d86df1c7a7010b8104354b27a0c33c27822d965
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2023-11-26 14:17:34 +0800

    fixed entity name

--- application/backend/src/entity/weapon.entity.ts
+++ application/backend/src/entity/weapon.entity.ts
@@ -1,7 +1,7 @@
 import { Entity,Column, PrimaryGeneratedColumn } from "typeorm";
 
-@Entity('allWeapons')
-export class Weapons {
+@Entity('allweapons')
+export class Weapons{
     
     @PrimaryGeneratedColumn()      
     id:number;
