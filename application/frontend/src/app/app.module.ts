1af62079b4770c8ce9c6fb2cc399e315049f9181
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2023-12-11 19:19:02 +0800

    Added new Components to Frontend

--- application/frontend/src/app/app.module.ts
+++ application/frontend/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TimerComponent } from './components/timer/timer.component';
+import { ClockComponent } from './components/clock/clock.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimerComponent,
+    ClockComponent
   ],
   imports: [
     BrowserModule,
