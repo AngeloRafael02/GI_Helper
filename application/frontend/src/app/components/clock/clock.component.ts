0abd6252a76350d172d0f8fe1ff7005e7e9e752b
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2023-12-12 09:44:36 +0800

    Added Clock Feature in Clock Component

--- application/frontend/src/app/components/clock/clock.component.ts
+++ application/frontend/src/app/components/clock/clock.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 
 @Component({
   selector: 'app-clock',
   templateUrl: '../../templates/clock.component.html',
   styleUrls: ['../../styles/clock.component.scss']
 })
-export class ClockComponent {
+export class ClockComponent implements OnInit {
+  public currentTime: string = '';
 
+  ngOnInit(): void {
+    setInterval(() => {
+      this.updateTime();
+    }, 1000);
+  }
+
+  updateTime() {
+    const currentDate = new Date();
+    let hours = currentDate.getHours();
+    const minutes = currentDate.getMinutes();
+    const seconds = currentDate.getSeconds();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+
+    hours = hours % 12;
+    hours = hours ? hours : 12; // Convert midnight (0 hours) to 12
+
+    const formattedHours = this.padZero(hours);
+    const formattedMinutes = this.padZero(minutes);
+    const formattedSeconds = this.padZero(seconds);
+
+    this.currentTime = `${formattedHours}:${formattedMinutes}:${formattedSeconds} ${ampm}`;
+  }
+  
+  padZero(value: number): string {
+    return value < 10 ? `0${value}` : `${value}`;
+  }
 }
