8f08336594342a28fc39824fd40912f447b40084
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2023-12-17 00:25:08 +0800

    created characters interface

---
+++ application/frontend/src/app/interfaces/characters.interface.ts
@@ -0,0 +1,18 @@
+export interface Characters {
+    id:number,
+    name:string,
+    star:number,
+    element:string,
+    type:string,
+    region:string,
+    domainname:string,
+    skillmats: string[],
+    enhancemats: string[],
+    gemmats: string[],
+    bossmats:string;
+    trouncemats:string;
+    localspecialty:string,
+    days: string[],
+    imgurl:string,
+
+}
