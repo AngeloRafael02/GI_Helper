2ef095d00ac557b03e4dd570dadff49fd4cfbe5d
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2023-12-17 00:25:33 +0800

    created weapons interface

---
+++ application/frontend/src/app/interfaces/weapons.interface.ts
@@ -0,0 +1,13 @@
+export interface Weapons{
+    id:number,
+    name:string,
+    star:number,
+    type:string,
+    stat:string,
+    domainname:string,
+    weaponmats: string[],
+    enhancemats1: string[],
+    enhancemats2: string[],
+    days: string[],
+    imgurl:string,
+}
