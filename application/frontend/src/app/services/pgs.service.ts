22ac5a23d0dd182e416d361e0bcbdb26da1b784d
Angelo Rafael F. Recio <recioangelorafael@gmail.com>
2024-01-02 20:42:51 +0800

    used string literals for URLRouting in methods

--- application/frontend/src/app/services/pgs.service.ts
+++ application/frontend/src/app/services/pgs.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Characters } from '../interfaces/characters.interface';
+import { Weapons } from '../interfaces/weapons.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +13,21 @@ export class PgsService {
     private http:HttpClient
   ) { }
 
+  private backendUrl = 'http://localhost:3000';
+
   private getDayCharacters(day:string):Observable<Characters[]>{
-    return this.http.get<Characters[]>('http://localhost:3000/day/'+day+"/Characters")
+    return this.http.get<Characters[]>(`${this.backendUrl}/day/${day}/Characters`)
   }
 
   private getACharacter(id:number):Observable<Characters[]>{
-    return this.http.get<Characters[]>('http://localhost:3000/character/'+id)
+    return this.http.get<Characters[]>(`${this.backendUrl}/character/'${id}`)
+  }
+
+  private getDayWeapons(day:string):Observable<Weapons[]>{
+    return this.http.get<Weapons[]>(`${this.backendUrl}/day/${day}/Weapons`)
+  }
+
+  private getAWeapon(id:number):Observable<Weapons[]>{
+    return this.http.get<Weapons[]>(`${this.backendUrl}/weapons/'${id}`)
   }
 }
